d3da293d93136b673d6ab4e3c6b5f00d926b93cd
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-06-24 10:34:44 +0530

    commit

--- 02_Basics/02_Objects.js
+++ 02_Basics/02_Objects.js
@@ -1,10 +1,38 @@
-console.log("hello from object file")
+// console.log("hello from object file")
 //#01
-const myobj = ({
-    name:"Harshita",
-    class:"btech",
-    rollno:"05001172022",
-    subject:"cse-ai"
-})
-console.log(myobj)
-console.table([myobj.name , myobj.class , myobj.rollno , myobj.subject])
+// const myobj = ({
+//     name:"Harshita",
+//     class:"btech",
+//     rollno:"05001172022",
+//     subject:"cse-ai"
+// })
+// console.log(myobj)
+// console.table([myobj.name , myobj.class , myobj.rollno , myobj.subject])
+
+// * There are 2 ways to make objects in JS: objects literals and singleton objects
+/*
+*  Object literals are the most common way to create objects in JavaScript. They are created using a simple and declarative syntax , can be created multiple times by simply defining the object literal structure each time you need a new instance
+*  Singleton objects is an object that is instantiated exactly once. It is a design pattern that ensures a class has only one instance and provides a global point of access to it.
+*/
+
+// object literal:
+let Person1 = {
+    Name: 'Harshita1',
+    age: 21,
+    print: function() {
+      console.log(`name is ${Person1.Name} and age is ${Person1.age}`);
+    }
+  };
+  let Person2 = {
+    Name: 'Harshita2',
+    age: 21,
+    print: function() {
+      console.log(`name is ${Person2.Name} and age is ${Person2.age}`);
+    }
+  };
+console.log(Person1)
+console.log(Person1.print())
+console.log(Person2)
+console.log(Person2.print())
+//singleton object:
+  
